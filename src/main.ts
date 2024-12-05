3b5996312da98da43b27864c9e72fca537c2c7c8
Thabang Gideon Magaola <gideon877@live.com>
2024-12-05 12:49:13 +0200

    added cors origin

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT ?? 4005;
-  await app.listen(PORT);
+	const app = await NestFactory.create(AppModule);
+	app.enableCors();
+
+	const PORT = process.env.PORT ?? 4005;
+	await app.listen(PORT);
 }
 bootstrap();

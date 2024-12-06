486d6535d3a4963c9bc9539a23c120e5d3a76886
Thabang Gideon Magaola <gideon877@live.com>
2024-12-06 12:59:17 +0200

    add logs to check app server

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	app.enableCors();
+	try {
+		const app = await NestFactory.create(AppModule);
+		app.enableCors();
 
-	const PORT = process.env.PORT ?? 4005;
-	await app.listen(PORT);
-	console.log(`Application is running on: http://localhost:${PORT}`);
+		const PORT = process.env.PORT ?? 4005;
+		await app.listen(PORT);
+		console.log(`Application is running on: http://localhost:${PORT}`);
 
+	} catch (error) {
+		console.error('Error starting the application:', error);
+
+	}
 }
 bootstrap();
